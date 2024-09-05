39b4f8b18faadb3095b978f060922f160d9106d7
DanielGallego <danielTHEgamer30@gmail.com>
2024-09-05 13:47:06 -0500

    Finalizacion de servicios

--- TallerFinal/TallerFinal1/Controllers/People.cs
+++ TallerFinal/TallerFinal1/Controllers/People.cs
@@ -4,11 +4,55 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using TallerFinal1.Clases;
+using TallerFinal1.Models;
 
 namespace TallerFinal1.Controllers
 {
     public class People : ApiController
     {
-   
+        //Post Insertar 
+        //Put Actualizar
+        //Delete Eliminar 
+        //Get obtener
+
+
+
+        [HttpPost]
+        [Route("Insertar")]
+        public string Insertar([FromBody] Person Person)
+        {
+            clsPeople people = new clsPeople();
+            people.person = Person;
+            return people.Insertar();
+        }
+
+        [HttpPut]
+        [Route("Actualizar")]
+        public string Actualizar([FromBody] Person Person)
+        {
+            clsPeople people = new clsPeople();
+            people.person = Person;
+            return people.Actualizar();
+        }
+
+        [HttpDelete]
+        [Route("Eliminar")]
+        public string Eliminar([FromBody] Person Person)
+        {
+            clsPeople people = new clsPeople();
+            people.person = Person;
+            return people.Eliminar();
+        }
+
+        [HttpGet]
+        [Route("Consultar")]
+        public Person Consultar(string ID)
+        {
+            clsPeople people = new clsPeople();
+            return people.Consultar(ID);
+        }
+
+
     }
 }
