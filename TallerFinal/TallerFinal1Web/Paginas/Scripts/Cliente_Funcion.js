72b01756620a113a0ef7f9fb0d4e48671f6294a7
DanielGallego <danielTHEgamer30@gmail.com>
2024-09-08 10:48:16 -0500

    Paginas y Scripts base cargados al proyecto

---
+++ TallerFinal/TallerFinal1Web/Paginas/Scripts/Cliente_Funcion.js
@@ -0,0 +1,38 @@
+﻿
+async function Ejecutar(Metodo, Funcion) {
+    //Se crea un objeto de la clase cliente con los datos de la interfaz
+    const cliente = new Cliente($("#txtDocumento").val(), $("#txtNombre").val(), $("#txtPrimerApellido").val(), $("#txtSegundoApellido").val(),
+        $("#txtDireccion").val(), $("#txtEmail").val(), $("#txtFechaNacimiento").val());
+    let URL = "http://localhost:54671/api/Clientes/" + Funcion;
+    EjecutarComandoServicio(Metodo, URL, cliente);
+}
+async function Consultar() {
+    let Documento = $("#txtDocumento").val();
+    URL = "http://localhost:54671/api/Clientes/ConsultarXDocumento?Documento=" + Documento;
+    //Invoco el servicio genérico
+    const cliente = await ConsultarServicio(URL);
+    if (cliente != null) {
+        $("#txtNombre").val(cliente.Nombre);
+        $("#txtPrimerApellido").val(cliente.PrimerApellido);
+        $("#txtSegundoApellido").val(cliente.SegundoApellido);
+        $("#txtDireccion").val(cliente.Direccion);
+        $("#txtFechaNacimiento").val(cliente.FechaNacimiento.split('T')[0]);
+        $("#txtEmail").val(cliente.Email);
+    }
+    else {
+        //Se presenta el error en un div de Mensaje
+        $("#dvMensaje").html("El cliente no existe en la base de datos");
+    }
+}
+
+class Cliente {
+    constructor(Documento, Nombre, PrimerApellido, SegundoApellido, Direccion, Email, FechaNacimiento) {
+        this.Documento = Documento;
+        this.Nombre = Nombre;
+        this.PrimerApellido = PrimerApellido;
+        this.SegundoApellido = SegundoApellido;
+        this.Direccion = Direccion;
+        this.Email = Email;
+        this.FechaNacimiento = FechaNacimiento;
+    }
+}
---
+++ TallerFinal/TallerFinal1Web/Paginas/Scripts/Comunes/CRUD.js
@@ -0,0 +1,39 @@
+﻿async function EjecutarComandoServicio(Metodo, URLServicio, Objeto) {
+    //Se crea un objeto de la clase cliente con los datos de la interfaz
+    try {
+        const Respuesta = await fetch(URLServicio,
+            {
+                method: Metodo,
+                mode: "cors",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(Objeto)
+            });
+        //Leer la respuesta
+        const Resultado = await Respuesta.json();
+        $("#dvMensaje").html(Resultado);
+    }
+    catch (error) {
+        //Se presenta el error en un div de Mensaje
+        $("#dvMensaje").html(error);
+    }
+}
+
+async function ConsultarServicio(URLServicio) {
+    //Para invocar el servicio, vamos a utilizar el método fetch de javascript, el cual me permite invocar una función en un servidor
+    try {
+        const Respuesta = await fetch(URLServicio,
+            {
+                method: "GET",
+                mode: "cors",
+                headers: { "Content-Type": "application/json" }
+            });
+        //Se traduce la respuesa a un objeto
+        const Resultado = await Respuesta.json();
+
+        return Resultado;
+    }
+    catch (error) {
+        //Se presenta el error en un div de Mensaje
+        $("#dvMensaje").html(error);
+    }
+}
